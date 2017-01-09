ff40447cb3711bb068f19e218a9987f9de9ccd5f
Jakub Sr <jakubsroka15@wp.pl>
2017-01-09 19:41:20 +0100

    modified:   app/src/main/java/com/example/piotr/zstwp/WfmNotificationFragment.java 	modified:   app/src/main/java/com/example/piotr/zstwp/WfmStartFragment.java 	modified:   app/src/main/res/layout/fragment_wfm_notification.xml 	modified:   app/src/main/res/layout/fragment_wfm_start.xml

--- app/src/main/java/com/example/piotr/zstwp/WfmNotificationFragment.java
+++ app/src/main/java/com/example/piotr/zstwp/WfmNotificationFragment.java
@@ -2,6 +2,8 @@ package com.example.piotr.zstwp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +19,24 @@ public class WfmNotificationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        //view = inflater.inflate(R.layout.fragment_wfm_main, container, false);
+        view = inflater.inflate(R.layout.fragment_wfm_notification, container, false);
 
-        return inflater.inflate(R.layout.fragment_wfm_notification, container, false);
+        //return inflater.inflate(R.layout.fragment_wfm_notification, container, false);
+        return view;
 
+    }
+
+    public void onBackPressed(){
+        Fragment fragmentStart = new WfmStartFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.notification, fragmentStart);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        ViewGroup mContainer = (ViewGroup) getActivity().findViewById(R.id.notification);
+        mContainer.removeAllViews();
     }
 
 
--- app/src/main/java/com/example/piotr/zstwp/WfmStartFragment.java
+++ app/src/main/java/com/example/piotr/zstwp/WfmStartFragment.java
@@ -2,11 +2,16 @@ package com.example.piotr.zstwp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import static android.content.ContentValues.TAG;
 
 public class WfmStartFragment extends Fragment implements View.OnClickListener {
 
@@ -18,14 +23,23 @@ public class WfmStartFragment extends Fragment implements View.OnClickListener {
 
         view = inflater.inflate(R.layout.fragment_wfm_start, container, false);
         loginButton = (Button) view.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
 
-        return inflater.inflate(R.layout.fragment_wfm_start, container, false);
+        //return inflater.inflate(R.layout.fragment_wfm_start, container, false);
+        return view;
     }
-
     @Override
     public void onClick(View v) {
-        //TODO
-        //Trzeba zrobić przejście do innego fragmentu w ramach zakładki
+        Fragment fragmentNotification = new WfmNotificationFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.start, fragmentNotification);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        ViewGroup mContainer = (ViewGroup) getActivity().findViewById(R.id.start);
+        mContainer.removeAllViews();
 
     }
 }
